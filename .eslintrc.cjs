34a14345c1616625e231fa373a12be68cf21e888
bj <kgorcab@gmail.com>
2023-01-10 18:52:28 +0900

    chore: eslint, prettier 설치 및 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        tabWidth: 2,
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  },
+};
